44b8fb8561b16fdd78852e4809ae05e8d909000d
Shreyas R <shreyas.r@avetoconsulting.com>
2026-01-19 17:58:40 +0530

    Code Changes

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 });
 
 pool.connect()
-  .then(() => console.log("✅ Connected to PostgreSQL"))
-  .catch((err) => console.error("❌ DB connection error:", err));
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => console.error("DB connection error:", err));
 
 module.exports = pool;
